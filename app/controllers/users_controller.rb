8deba9004f16a2f052c7c7793d8c2095b893af56
yashpreet <yashpreet@hoppr.com>
2012-12-26 18:38:42 +0530

    created login, logout with session authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,11 +40,12 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
+ skip_before_filter :authorize
     @user = User.new(params[:user])
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to @user, notice: 'User  was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: "new" }
@@ -60,7 +61,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to @user, notice: '#{@user.name} was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Addbook::Application.routes.draw do
+  get 'admin' => 'admin#index'
+  
+  controller :sessions do
+     get 'login' => :new
+     post 'login' => :create
+     delete 'logout' => :destroy
+  end
+
+  get "sessions/new"
+
+  get "sessions/create"
+
+  get "sessions/destroy"
+
   resources :users
 
 
@@ -51,7 +65,7 @@ Addbook::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'admin#index'
 
   # See how all your routes lay out with "rake routes"
 
