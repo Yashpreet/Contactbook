31f6acd7a5f406ed71a948466e7fe402169c939c
yashpreet <yashpreet@hoppr.com>
2012-12-26 16:01:45 +0530

    created addbook with user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Addbook::Application.config.secret_token = '64704c5fe81130a145635322429148f6ee1ed2b466d0f3d84822e7ee82bfe74f1c7e067e88ddec5a9772e982ee59a97404a9066bf20a787e78a12bd119d55ecd'
